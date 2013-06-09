e760ce3ef56231be870ea6dea5c34b344696873e
GermanoGiudici <germano.giudici@gmail.com>
2013-06-09 16:57:06 +0200

    first commit

---
+++ angularjs-client/Gruntfile.js
@@ -0,0 +1,64 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+            copy: {
+                main: {
+                    files: [
+                        {expand: true, src: ['app/**'], dest: '../src/main/webapp/'}
+                    ]
+                }
+            },
+            watch: {
+                scripts: {
+                    files: ['app/*'],
+                    options: {
+                        nospawn: true
+                    }
+                }
+            },
+
+            connect: {
+                test: {
+                    options: {
+                        port: 8000,
+                        base: '.'
+                    }
+                },
+                development: {
+                    options: {
+                        port: 8001,
+                        base: '.',
+                        keepalive: true
+                    }
+                }
+            },
+
+            karma: {
+                files: ['app/js/**/ *.js'],
+                unit: {
+                    configFile: 'config/karma.conf.js',
+                    singleRun: true
+                },
+                e2e: {
+                    configFile: 'config/karma-e2e.conf.js',
+                    singleRun: true
+                }
+            }
+
+        }
+    )
+    ;
+
+    grunt.event.on('watch', function (action, filepath) {
+        var destinationPath = "../src/main/webapp/";
+        grunt.log.writeln(filepath + " changed");
+        grunt.file.copy(filepath, destinationPath + filepath)
+    });
+
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.registerTask('develop', ['copy', 'watch']);
+    grunt.registerTask('test', ['connect:test', 'karma:unit', 'karma:e2e']);
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+}
---
+++ angularjs-client/app/js/controllers.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/* Controllers */
+/**
+ * @constructor
+ */
+function MyCtrl1() {}
+MyCtrl1.$inject = [];
+
+
+function MyCtrl2() {
+}
+MyCtrl2.$inject = [];
